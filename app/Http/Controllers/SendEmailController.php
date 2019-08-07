51b7662af43b7d315ecfae5fa331598e967100e4
mstfakdgn <mustafa/akiler@gmail.com>
2019-08-07 14:29:26 +0300

    asdas

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -2,24 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\EmailValidationRequest;
 
-
 class SendEmailController extends Controller
 {
-    public function sendMail(EmailValidationRequest $request) {
+    public function sendMail(EmailValidationRequest $request)
+    {
         $message = $request->message;
         $name = $request->name;
-        $email= $request->email;
-        
-        Mail::send('test', ['name' => $name, 'email' => $email,'message1'=>$message], function ($message){
-                $message->to('mohamednjikam25@hotmail.com');
+        $email = $request->email;
+
+        Mail::send('test', ['name' => $name, 'email' => $email, 'message1' => $message], function ($message) {
+            $message->to('mohamednjikam25@hotmail.com');
         });
+
         return redirect('/');
     }
-}  
-
-
-    
+}
